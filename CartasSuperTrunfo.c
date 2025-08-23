3d1aee49d183ed9baf40cba1e21788e51ca0d8ca
jessicarodrigs-design <jessicarodrigs@gmail.com>
2025-08-23 01:50:05 +0000

    CRIEI O NIVEL INICIANTE SUPER TRUNFO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -19,41 +19,87 @@ int main() {
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
 //Dados Carta 1
-char estado1 = 'A';
-char codigo1[] = "A01";
-char nomecidade1[] = "São Paulo";
-int populacao1 = 12345;
-float area1 = 132.54;
-float pib1 = 45.54;
-int pontosturisticos1 = 45;
+char estado1;
+char codigo1[4];
+char nomecidade1[20];
+int populacao1;
+float area1;
+float pib1;
+int pontosturisticos1;
 
 //Dados Carta 2
-char estado2 = 'B';
-char codigo2[] = "B02";
-char nomecidade2[] = "Rio de Janeiro";
-int populacao2 = 12453;
-float area2 = 145.55;
-float pib2 = 50.25;
-int pontosturisticos2 = 10;
+char estado2;
+char codigo2[4];
+char nomecidade2[20];
+int populacao2;
+float area2;
+float pib2;
+int pontosturisticos2;
+
+//Leitura Carta 1
+printf("\nCarta 1: \n");
+printf("Estado (A-H): ");
+scanf(" %c", &estado1);
+
+printf("Código da Carta (ex: A01): ");
+scanf("%s", codigo1);
+
+printf("Nome da Cidade: ");
+scanf(" %[^\n]", nomecidade1);
+
+printf("População: ");
+scanf("%d", &populacao1);
+
+printf("Área (em km²): ");
+scanf("%f", &area1);
+
+printf("PIB (em bilhões de reais): ");
+scanf("%f", &pib1);
+
+printf("Pontos Turisticos: ");
+scanf("%d", &pontosturisticos1);
+
+//Leitura Carta 2
+printf("\nCarta 2: \n");
+printf("Estado (A-H): ");
+scanf(" %c", &estado2);
+
+printf("Código da Carta (ex: B02): ");
+scanf("%s", codigo2);
+
+printf("Nome da Cidade: ");
+scanf(" %[^\n]", nomecidade2);
+
+printf("População: ");
+scanf("%d", &populacao2);
+
+printf("Área (em km²): ");
+scanf("%f", &area2);
+
+printf("PIB (em bilhões de reais): ");
+scanf("%f", &pib2);
+
+printf("Pontos Turisticos: ");
+scanf("%d", &pontosturisticos2);
 
 //Exibição Carta 1
-printf("Carta 1: \n");
+printf("\nCarta 1:\n");
 printf("Estado: %c\n", estado1);
-printf("Código: %s\n", codigo1);
+printf("Código da Carta: %s\n", codigo1);
 printf("Nome da Cidade: %s\n", nomecidade1);
 printf("População: %d\n", populacao1);
-printf("Area: %2.f km²\n", area1);
-printf("PIB: %2.f bilhoes de reis\n", pib1);
+printf("Área (em km²): %2f\n", area1);
+printf("PIB: %.2f bilhões de reais\n", pib1);
 printf("Pontos Turisticos: %d\n", pontosturisticos1);
 
 //Exibição Carta 2
-printf("Carta 2: \n");
+printf("\nCarta 2:\n");
 printf("Estado: %c\n", estado2);
-printf("Código: %s\n", codigo2);
+printf("Código da Carta: %s\n", codigo2);
 printf("Nome da Cidade: %s\n", nomecidade2);
 printf("População: %d\n", populacao2);
-printf("Area: %2.f km²\n", area2);
-printf("PIB: %2.f bilhoes de reis\n", pib2);
+printf("Área (em km²): %2f\n", area2);
+printf("PIB: %.2f bilhões de reais\n", pib2);
 printf("Pontos Turisticos: %d\n", pontosturisticos2);
 
 
